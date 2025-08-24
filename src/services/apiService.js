65c5d4d0d7565b3438494e6cc1f78d477ff9dea9
adamsirri1231 <adamsirri1233@outlook.com>
2025-08-24 03:10:41 +0100

    c

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://https://advantage-discovered-abraham-incident.trycloudflare.com/';
+const API_BASE_URL = 'https://advantage-discovered-abraham-incident.trycloudflare.com/';
 
 class ApiService {
   constructor() {
